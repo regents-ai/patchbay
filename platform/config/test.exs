8f0d948c3a40aede3ed1004044fbe0471a910f22
Sean Brennan <sean@regents.sh>
2026-09-06 06:51:51 -0400

    Bound test database connections for parallel worktrees

--- platform/config/test.exs
+++ platform/config/test.exs
@@ -11,7 +11,8 @@ config :patchbay, Patchbay.Repo,
   hostname: System.get_env("PATCHBAY_DB_HOST", "localhost"),
   database: "patchbay_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
-  pool_size: System.schedulers_online() * 2
+  # Keep parallel worktrees within the local PostgreSQL connection budget.
+  pool_size: 8
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
