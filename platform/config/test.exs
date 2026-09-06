44179333157700c25a1bc063d723e6424727cceb
Sean Brennan <sean@regents.sh>
2026-09-06 07:31:16 -0400

    Match isolated test browser origins to their assigned port

--- platform/config/test.exs
+++ platform/config/test.exs
@@ -1,4 +1,5 @@
 import Config
+browser_port = String.to_integer(System.get_env("PORT", "4002"))
 
 # Configure your database
 #
@@ -17,7 +18,9 @@ config :patchbay, Patchbay.Repo,
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :patchbay, PatchbayWeb.Endpoint,
-  http: [ip: {127, 0, 0, 1}, port: String.to_integer(System.get_env("PORT", "4002"))],
+  url: [host: "127.0.0.1", port: browser_port],
+  http: [ip: {127, 0, 0, 1}, port: browser_port],
+  check_origin: ["http://127.0.0.1:#{browser_port}"],
   secret_key_base: "veQzbAXuIb79+jLk5QWx2Jq4Gy/5WzZbQRfCk77MHXxP2IUiKWLI5vDOixz/wQlP",
   server: false
 
